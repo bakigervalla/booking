e44b415afd0702d71cde7165a3ab9ef0293dd7e9
Baki <bakigervalla@gmail.com>
2022-12-16 20:34:02 +0100

    display content, remove ferguson service

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { BrowserView, MobileView } from 'react-device-detect';
+import React, { useEffect, useState, useRef } from "react";
+import { BrowserView, MobileView } from "react-device-detect";
 
 import {
   useVehicles,
@@ -8,52 +8,72 @@ import {
   removeService,
   setServiceAgreement,
   setStep,
-} from '../context/VehicleState'
+} from "../context/VehicleState";
 
-import { VehicleInfo } from './index'
+import { VehicleInfo } from "./index";
 
-import Alert from '../layout/Alert'
-import '../layout/css/vehicle.css'
-import Spinner from '../layout/Spinner'
-import '../layout/css/service.css'
+import Alert from "../layout/Alert";
+import "../layout/css/vehicle.css";
+import Spinner from "../layout/Spinner";
+import "../layout/css/service.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle, faCheck, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlusCircle,
+  faCheck,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Services = ({ workshop }) => {
-  const [vehicleState, vehicleDispatch] = useVehicles()
-  const { vehicles, categories, services, filtered, serviceAgreement, isEUKontroll, loading, step, error } = vehicleState
-  const [showMore, setShowMore] = useState([false])
-  const [descr, toggleDescription] = useState()
-  const [chkDesc, toggleCheckboxDesc] = useState()
+  const [vehicleState, vehicleDispatch] = useVehicles();
+  const {
+    vehicles,
+    categories,
+    services,
+    filtered,
+    serviceAgreement,
+    isEUKontroll,
+    loading,
+    step,
+    error,
+  } = vehicleState;
+  const [showMore, setShowMore] = useState([false]);
+  const [descr, toggleDescription] = useState();
+  const [chkDesc, toggleCheckboxDesc] = useState();
 
-  const topRef = useRef(null)
+  const topRef = useRef(null);
 
   useEffect(() => {
-    getServices(vehicleDispatch, workshop.workshop_id, isEUKontroll)
-  }, [isEUKontroll, vehicleDispatch, workshop.workshop_id])
+    getServices(vehicleDispatch, workshop.workshop_id, isEUKontroll);
+  }, [isEUKontroll, vehicleDispatch, workshop.workshop_id]);
 
   useEffect(() => {
-    topRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
-  }, [categories])
+    topRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  }, [categories]);
 
   function Item({ item }) {
     return item.is_active ? (
       <>
-        <div className='flex grid-cols-2 w-full'>
-          <div className='grid w-11/12'
+        <div className="flex grid-cols-2 w-full">
+          <div
+            className="grid w-11/12"
             onClick={() => {
-              addService(vehicleDispatch, item)
-            }}>
-            <span className='text-start'>{item.name}</span>
+              addService(vehicleDispatch, item);
+            }}
+          >
+            <span className="text-start">{item.name}</span>
           </div>
-          <div className='grid grid-cols-2 place-items-center'>
+          <div className="grid grid-cols-2 place-items-center">
             <div
               onClick={() => {
-                addService(vehicleDispatch, item)
-              }}>
+                addService(vehicleDispatch, item);
+              }}
+            >
               {item.selected ? (
-                <FontAwesomeIcon icon={faCheck} className="icon selected-service" />
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="icon selected-service"
+                />
               ) : (
                 <FontAwesomeIcon icon={faPlusCircle} className="icon" />
               )}
@@ -62,7 +82,9 @@ const Services = ({ workshop }) => {
               <FontAwesomeIcon
                 icon={faInfoCircle}
                 className="icon-left"
-                onClick={(e) => toggleDescription(descr !== item.id ? item.id : -1)}
+                onClick={(e) =>
+                  toggleDescription(descr !== item.id ? item.id : -1)
+                }
               />
               <div className="arrow"></div>
               <div className="info">
@@ -71,15 +93,15 @@ const Services = ({ workshop }) => {
             </div>
           </div>
         </div>
-        <div className={`service-desc ${descr === item.id ? 'show' : null}`}>
+        <div className={`service-desc ${descr === item.id ? "show" : null}`}>
           {item.description}
         </div>
       </>
     ) : (
       <div className="inactive">
-        <span className=' text-left'>{item.name}</span>
+        <span className=" text-left">{item.name}</span>
       </div>
-    )
+    );
   }
 
   const StandardTemplate = ({ id }) => {
@@ -88,11 +110,11 @@ const Services = ({ workshop }) => {
         {services
           .filter((itm) => itm.category_id === id)
           .map((service, id) => {
-            return <Item key={service.id} item={service} />
+            return <Item key={service.id} item={service} />;
           })}
       </div>
-    )
-  }
+    );
+  };
 
   const ExpandedTemplate = ({ id }) => {
     return (
@@ -102,7 +124,7 @@ const Services = ({ workshop }) => {
             .filter((itm) => itm.category_id === id)
             .slice(0, 9)
             .map((service, id) => {
-              return <Item key={service.id} item={service} />
+              return <Item key={service.id} item={service} />;
             })}
           {showMore[id] ? (
             <div className="hidden-items">
@@ -110,26 +132,25 @@ const Services = ({ workshop }) => {
                 .filter((itm) => itm.category_id === id)
                 .slice(9)
                 .map((service, id) => {
-                  return <Item key={service.id} item={service} />
+                  return <Item key={service.id} item={service} />;
                 })}
             </div>
           ) : null}
         </div>
         <span className="show-more" onClick={() => toggleShowMore(id)}>
-          {showMore[id] ? 'Show less' : 'Show more'}
+          {showMore[id] ? "Show less" : "Show more"}
         </span>
       </>
-    )
-  }
+    );
+  };
 
   const toggleShowMore = (key) => {
-    let newArr = [...showMore]
-    newArr[key] = !showMore[key]
-    setShowMore(newArr)
-  }
+    let newArr = [...showMore];
+    newArr[key] = !showMore[key];
+    setShowMore(newArr);
+  };
   return (
     <div className="w2">
-
       {error && <Alert type="error" message={error} />}
       {loading && <Spinner />}
 
@@ -137,49 +158,39 @@ const Services = ({ workshop }) => {
 
       {/* Checkbox */}
       <BrowserView>
-        <div ref={topRef} className="grid grid-cols-3 gap-4 content-start h-24 justify-center items-center">
+        <div
+          ref={topRef}
+          className="grid grid-cols-3 gap-4 content-start h-24 justify-center items-center"
+        >
           <div className="w-96 grid-container">
             <div className="first-service-heading">
-              <div className='flex row items-center ml-4'>
-                <input
-                  type="checkbox"
-                  checked={serviceAgreement}
-                  onChange={() => setServiceAgreement(vehicleDispatch, !serviceAgreement)}
-                />
-                <p>Fragus serviceavtale</p>
-                <div className="tooltip" id="third">
-                  <FontAwesomeIcon
-                    icon={faInfoCircle}
-                    className="icon-left"
-                    onClick={(e) => toggleCheckboxDesc(!chkDesc)}
-                  />
-                  <div className={`service-desc ${chkDesc ? 'show' : null}`}>
-                    <span>Har du Fragus serviceavtale? Huk av for det her.</span>
-                  </div>
-
-
-                </div>
+              <div className={`service-desc ${chkDesc ? "show" : null}`}>
+                <span>Har du Fragus serviceavtale? Huk av for det her.</span>
               </div>
-
             </div>
-
           </div>
 
           <div>
-            <h2 className="title pt-6 whitespace-nowrap">Hva gjelder henvendelsen?</h2>
+            <h2 className="title pt-6 whitespace-nowrap">
+              Hva gjelder henvendelsen?
+            </h2>
           </div>
-
         </div>
       </BrowserView>
       <MobileView>
-        <div ref={topRef} className="grid grid-rows-2 gap-4 content-start h-28 justify-center items-center">
+        <div
+          ref={topRef}
+          className="grid grid-rows-2 gap-4 content-start h-28 justify-center items-center"
+        >
           <div className="w-96 grid-container">
             <div className="first-service-heading">
-              <div className='flex row items-center ml-4'>
+              <div className="flex row items-center ml-4">
                 <input
                   type="checkbox"
                   checked={serviceAgreement}
-                  onChange={() => setServiceAgreement(vehicleDispatch, !serviceAgreement)}
+                  onChange={() =>
+                    setServiceAgreement(vehicleDispatch, !serviceAgreement)
+                  }
                 />
                 <p>Fragus serviceavtale</p>
                 <div className="tooltip" id="third">
@@ -188,26 +199,25 @@ const Services = ({ workshop }) => {
                     className="icon-left"
                     onClick={(e) => toggleCheckboxDesc(!chkDesc)}
                   />
-                  <div className={`service-desc ${chkDesc ? 'show' : null}`}>
-                    <span>Har du Fragus serviceavtale? Huk av for det her.</span>
+                  <div className={`service-desc ${chkDesc ? "show" : null}`}>
+                    <span>
+                      Har du Fragus serviceavtale? Huk av for det her.
+                    </span>
                   </div>
-
-
                 </div>
               </div>
-
             </div>
-
           </div>
 
           <div>
-            <h2 className="title pt-6 whitespace-nowrap text-center">Hva gjelder henvendelsen?</h2>
+            <h2 className="title pt-6 whitespace-nowrap text-center">
+              Hva gjelder henvendelsen?
+            </h2>
           </div>
-
         </div>
       </MobileView>
 
-      <div className='grid grid-cols-1 gap-4 h-full content-start'>
+      <div className="grid grid-cols-1 gap-4 h-full content-start">
         <div className="services h-full">
           {categories &&
             categories.map((category, key) => {
@@ -215,15 +225,15 @@ const Services = ({ workshop }) => {
                 <div key={key} className="service-box w-full md:w-[23.2%]">
                   <h5>{category.name}</h5>
                   {services.filter((itm) => {
-                    if (itm.category_id === category.id) return true
-                    return false
+                    if (itm.category_id === category.id) return true;
+                    return false;
                   }).length <= 9 ? (
                     <StandardTemplate id={category.id} />
                   ) : (
                     <ExpandedTemplate id={category.id} />
                   )}
                 </div>
-              )
+              );
             })}
         </div>
 
@@ -238,13 +248,13 @@ const Services = ({ workshop }) => {
                       <span key={item.id}>{item.name}</span>
                       <i
                         onClick={() => {
-                          removeService(vehicleDispatch, item)
+                          removeService(vehicleDispatch, item);
                         }}
                       >
                         x
                       </i>
                     </div>
-                  )
+                  );
                 })
               ) : (
                 <span></span>
@@ -252,40 +262,40 @@ const Services = ({ workshop }) => {
             </div>
           </div>
           <div className="buttons">
-            <input className='rounded-lg'
+            <input
+              className="rounded-lg"
               type="button"
               value="TILBAKE"
               onClick={() => {
-                setStep(vehicleDispatch, step - 1, '/booking/services')
+                setStep(vehicleDispatch, step - 1, "/booking/services");
               }}
             />
-            <input className='rounded-lg'
+            <input
+              className="rounded-lg"
               type="button"
               value="NESTE"
               disabled={filtered.length === 0}
               onClick={() => {
-                setStep(vehicleDispatch, 3, '/booking/services')
+                setStep(vehicleDispatch, 3, "/booking/services");
               }}
             />
           </div>
         </div>
 
-        <span className='text-center'>
-          Kontakt oss på tlf.{' '}
+        <span className="text-center">
+          Kontakt oss på tlf.{" "}
           <a className="link" href={`tel:${workshop.phone}`}>
             {workshop.phone}
-          </a>{' '}
-          eller{' '}
+          </a>{" "}
+          eller{" "}
           <a className="link" href={`mailto:${workshop.email}`}>
             {workshop.email}
-          </a>{' '}
+          </a>{" "}
           eller bruk skjemaet over.
         </span>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
